2513eeb104f2f26c4dd3b9e26a051ba11197d150
Jiezju <21725100@zju.edu.cn>
2022-01-09 14:15:06 +0800

    [AI virtual keyboard]

---
+++ opencv_project/AIVirtualKeyboard/main.py
@@ -0,0 +1,109 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+import numpy as np
+import cvzone
+from pynput.keyboard import Controller
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# 手部检测器， 置信度为 0.8
+detector = HandDetector(detectionCon=0.8)
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+finalText = ""
+
+keyboard = Controller()
+
+
+def drawAll(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
+                          20, rt=0)
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+    return img
+
+
+# draw with alpha 透明度
+def drawAllWithAlpha(img, buttonList):
+    imgNew = np.zeros_like(img, np.uint8)
+    for button in buttonList:
+        x, y = button.pos
+        # 添加围绕 长方形的 角框
+        cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
+                          20, rt=0)
+        cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),
+                      (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgNew, button.text, (x + 40, y + 60),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+
+    out = img.copy()
+    alpha = 0.5
+    mask = imgNew.astype(bool)
+    print(mask.shape)
+    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+    return out
+
+# 按钮类
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+
+# 创建键盘
+buttonList = []
+for i in range(len(keys)):
+    for j, key in enumerate(keys[i]):
+        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
+
+while True:
+    success, img = cap.read()
+    # img = detector.findHands(img)
+    # lmList, bboxInfo = detector.findPosition(img)
+    hands, img = detector.findHands(img)
+    lmList = []
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList = hand1["lmList"]  # List of 21 Landmark points
+        bboxInfo = hand1["bbox"]  # Bounding box info x,y,w,h
+
+    img = drawAll(img, buttonList)
+
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+
+            # lmList[8] 表示食指头
+            if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
+                cv2.rectangle(img, (x - 5, y - 5), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
+                cv2.putText(img, button.text, (x + 20, y + 65),
+                            cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                # 计算 第 8 点 到 第 12 点之间的距离
+                l, _, _ = detector.findDistance(lmList[8], lmList[12], img)
+                print(l)
+
+                # when clicked 当距离小于 30 时，作为点击判别
+                if l < 30:
+                    keyboard.press(button.text)
+                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, button.text, (x + 20, y + 65),
+                                cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                    finalText += button.text
+                    sleep(0.15)
+
+    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
+    cv2.putText(img, finalText, (60, 430),
+                cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
