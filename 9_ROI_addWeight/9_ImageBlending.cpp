7eed09bff4b1f794e079d4e1e6d9cc088890a01b
Jiezju <21725100@zju.edu.cn>
2022-01-22 00:15:43 +0800

    [opencv] update code

---
+++ 9_ROI_addWeight/9_ImageBlending.cpp
@@ -0,0 +1,159 @@
+//--------------------------------------【程序说明】-------------------------------------------
+//		程序说明：《OpenCV3编程入门》OpenCV2版书本配套示例程序25
+//		程序描述：初级图像混合
+//		开发测试所用IDE版本：Visual Studio 2010
+//		开发测试所用OpenCV版本：	3.0 beta
+//		2014年11月 Created by @浅墨_毛星云
+//		2014年12月 Revised by @浅墨_毛星云
+//------------------------------------------------------------------------------------------------
+
+
+//---------------------------------【头文件、命名空间包含部分】-------------------------------
+//		描述：包含程序所使用的头文件和命名空间
+//------------------------------------------------------------------------------------------------
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+//-----------------------------------【全局函数声明部分】--------------------------------------
+//	描述：全局函数声明
+//-----------------------------------------------------------------------------------------------
+bool  ROI_AddImage();
+bool  LinearBlending();
+bool  ROI_LinearBlending();
+void   ShowHelpText();
+
+//-----------------------------------【main( )函数】--------------------------------------------
+//	描述：控制台应用程序的入口函数，我们的程序从这里开始
+//-----------------------------------------------------------------------------------------------
+int main(   )
+{
+    system("color 6F");
+
+    ShowHelpText();
+
+    if(ROI_AddImage( )&& LinearBlending( )&&ROI_LinearBlending( ))
+    {
+        cout<<endl<<"\n运行成功，得出了需要的图像";
+    }
+
+    waitKey(0);
+    return 0;
+}
+
+
+//-----------------------------------【ShowHelpText( )函数】----------------------------------
+//		 描述：输出一些帮助信息
+//----------------------------------------------------------------------------------------------
+void ShowHelpText()
+{
+    //输出欢迎信息和OpenCV版本
+    printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION );
+    printf("\n\n  ----------------------------------------------------------------------------\n");
+}
+
+
+
+
+//----------------------------------【ROI_AddImage( )函数】----------------------------------
+// 函数名：ROI_AddImage（）
+//	描述：利用感兴趣区域ROI实现图像叠加
+//----------------------------------------------------------------------------------------------
+bool  ROI_AddImage()
+{
+
+    // 【1】读入图像
+    Mat srcImage1= imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/dota_pa.jpg");
+    Mat logoImage= imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/dota_logo.jpg");
+    if( !srcImage1.data ) { printf("读取srcImage1错误~！ \n"); return false; }
+    if( !logoImage.data ) { printf("读取logoImage错误~！ \n"); return false; }
+
+    // 【2】定义一个Mat类型并给其设定ROI区域
+    // Rect(左上角坐标， 矩形宽高)
+    Mat imageROI= srcImage1(Rect(200,250,logoImage.cols,logoImage.rows));
+
+    // 另一种写法
+    // Mat imageROI= srcImage1(Range(250, 250+logoImage.rows), Range(200, 200+logoImage.cols));
+
+    // 【3】加载掩模（必须是灰度图）
+    Mat mask= imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/dota_logo.jpg",0);
+
+    //【4】将掩膜拷贝到ROI mask -> imageROI
+    logoImage.copyTo(imageROI,mask);
+
+    // 【5】显示结果
+    namedWindow("<1>利用ROI实现图像叠加示例窗口");
+    imshow("<1>利用ROI实现图像叠加示例窗口",srcImage1);
+
+    return true;
+}
+
+
+//---------------------------------【LinearBlending（）函数】-------------------------------------
+// 函数名：LinearBlending（）
+// 描述：利用cv::addWeighted（）函数实现图像线性混合
+// g = (1-a)*f_1 + a*f_2
+// 画面叠化
+//--------------------------------------------------------------------------------------------
+bool  LinearBlending()
+{
+    //【0】定义一些局部变量
+    double alphaValue = 0.5;
+    double betaValue;
+    Mat srcImage2, srcImage3, dstImage;
+
+    // 【1】读取图像 ( 两幅图片需为同样的类型和尺寸 )
+    srcImage2 = imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/mogu.jpg");
+    srcImage3 = imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/rain.jpg");
+
+    if( !srcImage2.data ) { printf("读取srcImage2错误！ \n"); return false; }
+    if( !srcImage3.data ) { printf("读取srcImage3错误！ \n"); return false; }
+
+    // 【2】进行图像混合加权操作
+    betaValue = ( 1.0 - alphaValue );
+    // dst = src1 * alphaValue + src2 * betaValue + gamma
+    addWeighted( srcImage2, alphaValue, srcImage3, betaValue, 0.0, dstImage);
+
+    // 【3】显示原图窗口
+    imshow( "<2>线性混合示例窗口【原图】", srcImage2 );
+    imshow( "<3>线性混合示例窗口【效果图】", dstImage );
+
+    return true;
+
+}
+
+//---------------------------------【ROI_LinearBlending（）】-------------------------------------
+// 函数名：ROI_LinearBlending（）
+// 描述：线性混合实现函数,指定区域线性图像混合.利用cv::addWeighted（）函数结合定义
+//			  感兴趣区域ROI，实现自定义区域的线性混合
+//--------------------------------------------------------------------------------------------
+bool  ROI_LinearBlending()
+{
+
+    //【1】读取图像
+    Mat srcImage4= imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/dota_pa.jpg",1);
+    Mat logoImage= imread("/home/bright/CLionProjects/opencv/9_ROI_addWeight/dota_logo.jpg");
+
+    if( !srcImage4.data ) { printf("读取srcImage4错误~！ \n"); return false; }
+    if( !logoImage.data ) { printf("读取logoImage错误~！ \n"); return false; }
+
+    //【2】定义一个Mat类型并给其设定ROI区域
+    Mat imageROI;
+    //方法一
+    imageROI= srcImage4(Rect(200,250,logoImage.cols,logoImage.rows));
+    //方法二
+    //imageROI= srcImage4(Range(250,250+logoImage.rows),Range(200,200+logoImage.cols));
+
+    //【3】将logo加到原图上
+    addWeighted(imageROI,0.5,logoImage,0.3,0.,imageROI);
+
+    //【4】显示结果
+    imshow("<4>区域线性图像混合示例窗口",srcImage4);
+
+    return true;
+}
+
